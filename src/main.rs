d8b08e02b8d55612157926e634a5ebeb1058fd27
Felipe Lima <gl.feelipe@gmail.com>
2026-02-21 18:35:52 -0300

    Learning Rust

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn convert_to_int(data_input: & String) -> i32{
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+
+}
+
+fn main(){
+    let mut number1 = String::new();
+    io::stdin().read_line(&mut number1).expect("erro de entrada");
+    let mut number2 = String::new();
+    io::stdin().read_line(&mut number2).expect("erro de entrada");
+    if convert_to_int(&number1) > convert_to_int(&number2) {
+        println!("numero {} é maior que o {} ", number1, number2);
+    } else {
+        println!("o numero {} <= {}", number1, number2);
+    }
+}
